c23504c1cefdef3c437b82086d61b56b7ffbf1f9
fiskindal <mehmetfiskindal@gmail.com>
2022-10-08 21:07:44 +0300

    temizlik

--- lib/fishop_firebase.dart
+++ lib/fishop_firebase.dart
@@ -1,8 +1,6 @@
 library fishop_firebase;
 
 export 'base/model/address_book/address_book.dart';
-export 'base/model/card.dart';
-export 'base/model/gift_card.dart';
 export 'base/model/product/product.dart';
 export 'base/model/profile.dart';
 export 'base/model/user.dart';
