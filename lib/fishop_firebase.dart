3e6afb8265cc1c3a733d57dd9c956c4350e0f0c9
fiskindal <mehmetfiskindal@gmail.com>
2022-12-12 13:08:30 +0300

    export

--- lib/fishop_firebase.dart
+++ lib/fishop_firebase.dart
@@ -5,3 +5,5 @@ export 'base/model/profile/profile.dart';
 export 'base/model/user/user.dart';
 
 export 'base/services/auth_service.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
